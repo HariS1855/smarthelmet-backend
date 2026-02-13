27b0d157bcabb65c136fbdad5244b435c9fb6fa6
abishekpandi829 <210822205002@kingsedu.ac.in>
2026-02-13 22:28:41 +0530

    Update AlertController.java

--- src/main/java/com/smarthelmet/helmet_backend/controller/AlertController.java
+++ src/main/java/com/smarthelmet/helmet_backend/controller/AlertController.java
@@ -53,7 +53,7 @@ public class AlertController {
                     List<Worker> allWorkers = workerRepository.findAll();
                     for (Worker w : allWorkers) {
                         if (!w.getHelmetId().equals(worker.getHelmetId())) {
-                        notificationService.sendAlertToWorker(w, alert);
+                        notificationService.sendAlertToWorker(w, worker, alert);
                         }
                     }
 
